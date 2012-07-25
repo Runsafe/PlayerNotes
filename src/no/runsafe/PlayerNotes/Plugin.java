51f6d6f61502f02b42d00e5618e98b65a2f725b2
Morten Nilsen <morten@runsafe.no>
2012-07-25 20:10:01 +0200

    Use global runsafe db config

--- src/no/runsafe/PlayerNotes/Plugin.java
+++ src/no/runsafe/PlayerNotes/Plugin.java
@@ -1,14 +1,11 @@
 package no.runsafe.PlayerNotes;
 
 import no.runsafe.framework.RunsafePlugin;
-import no.runsafe.framework.configuration.IConfigurationFile;
 import no.runsafe.PlayerNotes.database.*;
 import no.runsafe.PlayerNotes.command.*;
 import no.runsafe.PlayerNotes.event.*;
 
-import java.io.InputStream;
-
-public class Plugin extends RunsafePlugin implements IConfigurationFile
+public class Plugin extends RunsafePlugin
 {
 	@Override
 	protected void PluginSetup()
@@ -16,18 +13,6 @@ public class Plugin extends RunsafePlugin implements IConfigurationFile
 		addComponent(NoteRepository.class);
 		addComponent(NoteManager.class);
 		addComponent(getInstance(NoteCommand.class));
-		addComponent(PlayerLogin.class);
-	}
-
-	@Override
-	public String getConfigurationPath()
-	{
-		return "plugins/" + this.getName() + "/config.yml";
-	}
-
-	@Override
-	public InputStream getDefaultConfiguration()
-	{
-		return getResource("defaults.yml");
+		addComponent(PlayerJoin.class);
 	}
 }
