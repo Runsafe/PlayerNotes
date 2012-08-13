e1cff3d08a88dabb717b0d37cd1c30bfb82b4c33
Morten Nilsen <morten@runsafe.no>
2012-08-14 00:09:45 +0200

    Go asynchronous!

--- src/no/runsafe/PlayerNotes/event/PlayerJoin.java
+++ src/no/runsafe/PlayerNotes/event/PlayerJoin.java
@@ -1,12 +1,13 @@
 package no.runsafe.PlayerNotes.event;
 
 import no.runsafe.PlayerNotes.NoteManager;
+import no.runsafe.framework.event.IAsyncEvent;
 import no.runsafe.framework.event.player.IPlayerJoinEvent;
 import no.runsafe.framework.event.player.IPlayerLoginEvent;
 import no.runsafe.framework.server.event.player.RunsafePlayerJoinEvent;
 import no.runsafe.framework.server.event.player.RunsafePlayerLoginEvent;
 
-public class PlayerJoin implements IPlayerJoinEvent
+public class PlayerJoin implements IPlayerJoinEvent, IAsyncEvent
 {
 	public PlayerJoin(NoteManager manager)
 	{
