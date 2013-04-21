9c4c85cf072fd4c8023693e551e5a421d357af4d
Kruithne <kruithne@gmail.com>
2013-04-21 06:13:32 +0100

    Added ability to randomly generate a player tag, for science.

--- src/no/runsafe/PlayerNotes/command/SetCommand.java
+++ src/no/runsafe/PlayerNotes/command/SetCommand.java
@@ -1,6 +1,7 @@
 package no.runsafe.PlayerNotes.command;
 
 import no.runsafe.PlayerNotes.NoteManager;
+import no.runsafe.PlayerNotes.RandomGenerator;
 import no.runsafe.framework.command.AsyncCommand;
 import no.runsafe.framework.server.ICommandExecutor;
 import no.runsafe.framework.server.RunsafeServer;
@@ -11,10 +12,11 @@ import java.util.HashMap;
 
 public class SetCommand extends AsyncCommand
 {
-	public SetCommand(NoteManager manager, IScheduler scheduler)
+	public SetCommand(NoteManager manager, IScheduler scheduler, RandomGenerator randomGenerator)
 	{
 		super("set", "Sets a note for the given player", "runsafe.note.set.<tier>", scheduler, "tier", "note");
 		this.manager = manager;
+		this.randomGenerator = randomGenerator;
 		captureTail();
 	}
 
@@ -22,9 +24,11 @@ public class SetCommand extends AsyncCommand
 	public String OnAsyncExecute(ICommandExecutor executor, HashMap<String, String> params)
 	{
 		RunsafePlayer target = RunsafeServer.Instance.getPlayer(params.get("player"));
-		manager.setNoteForPlayer(target, params.get("tier"), params.get("note"));
+		String note = (params.get("note").equalsIgnoreCase("random") ? randomGenerator.getRandomTag() : params.get("note"));
+		manager.setNoteForPlayer(target, params.get("tier"), note);
 		return String.format(("%s note set for %s."), params.get("tier"), target.getPrettyName());
 	}
 
 	private final NoteManager manager;
+	private RandomGenerator randomGenerator;
 }
