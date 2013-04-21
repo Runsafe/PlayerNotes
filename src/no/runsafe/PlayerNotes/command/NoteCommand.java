3e3c36e8443276282560d87649834b590343243e
Kruithne <kruithne@gmail.com>
2013-04-21 06:18:42 +0100

    Fixed missing injection for RandomGenerator.

--- src/no/runsafe/PlayerNotes/command/NoteCommand.java
+++ src/no/runsafe/PlayerNotes/command/NoteCommand.java
@@ -1,16 +1,17 @@
 package no.runsafe.PlayerNotes.command;
 
 import no.runsafe.PlayerNotes.NoteManager;
+import no.runsafe.PlayerNotes.RandomGenerator;
 import no.runsafe.framework.command.Command;
 import no.runsafe.framework.timer.IScheduler;
 
 public class NoteCommand extends Command
 {
-	public NoteCommand(NoteManager manager, IScheduler scheduler)
+	public NoteCommand(NoteManager manager, IScheduler scheduler, RandomGenerator randomGenerator)
 	{
 		super("note", "Manage player notes", "runsafe.notes", "player");
 
-		addSubCommand(new SetCommand(manager, scheduler));
+		addSubCommand(new SetCommand(manager, scheduler, randomGenerator));
 		addSubCommand(new ClearCommand(manager, scheduler));
 		addSubCommand(new ListCommand(manager, scheduler));
 	}
