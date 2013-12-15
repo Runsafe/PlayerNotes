bf0aabfaadd133886d820bf9e1fe633adbf7055d
Morten Nilsen <morten@runsafe.no>
2013-12-15 12:50:42 +0100

    Declare features used

--- src/no/runsafe/PlayerNotes/Plugin.java
+++ src/no/runsafe/PlayerNotes/Plugin.java
@@ -4,12 +4,21 @@ import no.runsafe.PlayerNotes.command.NoteCommand;
 import no.runsafe.PlayerNotes.database.NoteRepository;
 import no.runsafe.PlayerNotes.event.PlayerJoin;
 import no.runsafe.framework.RunsafeConfigurablePlugin;
+import no.runsafe.framework.features.Commands;
+import no.runsafe.framework.features.Database;
+import no.runsafe.framework.features.Events;
 
 public class Plugin extends RunsafeConfigurablePlugin
 {
 	@Override
 	protected void PluginSetup()
 	{
+		// Framework features
+		addComponent(Events.class);
+		addComponent(Commands.class);
+		addComponent(Database.class);
+
+		// Plugin components
 		addComponent(NoteRepository.class);
 		addComponent(NoteManager.class);
 		addComponent(NoteCommand.class);
