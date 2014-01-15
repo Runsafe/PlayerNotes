364eec54abdd5bb47dd8aec76fbfb280d9a420c7
Morten Nilsen <morten@runsafe.no>
2014-01-15 22:11:27 +0100

    Upstream API changes

--- src/no/runsafe/PlayerNotes/command/NoteCommand.java
+++ src/no/runsafe/PlayerNotes/command/NoteCommand.java
@@ -4,13 +4,13 @@ import no.runsafe.PlayerNotes.NoteManager;
 import no.runsafe.framework.api.IScheduler;
 import no.runsafe.framework.api.IServer;
 import no.runsafe.framework.api.command.Command;
-import no.runsafe.framework.api.command.argument.PlayerArgument;
+import no.runsafe.framework.api.command.argument.AnyPlayerRequired;
 
 public class NoteCommand extends Command
 {
 	public NoteCommand(NoteManager manager, IScheduler scheduler, IServer server)
 	{
-		super("note", "Manage player notes", "runsafe.notes", new PlayerArgument());
+		super("note", "Manage player notes", "runsafe.notes", new AnyPlayerRequired());
 
 		addSubCommand(new SetCommand(manager, scheduler, server));
 		addSubCommand(new ClearCommand(manager, scheduler, server));
